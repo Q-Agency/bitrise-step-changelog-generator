51f0f7605ec0a6c8cd21f7c03004bf70a550d90a
dmesek <dominik.mesek@q.agency>
2022-11-28 14:00:43 +0100

    added tag and new script

--- step.sh
+++ step.sh
@@ -1,33 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+environment="staging"
 
+newest_tag=`git tag | grep $environment | tail -n 1`
+if [ -z $newest_tag ]; then
+   echo "No $environment tag found."
+   exit 1
+fi
 
-#!/usr/bin/env bash
-# fail if any commands fails
+tag_count=`git tag | grep $environment | wc -l`
+second_newest_tag=`git tag | grep $environment | tail -n 2 | head -n 1`
+if [ $tag_count -eq "1" ]; then
+   changelog=`git log --pretty=format:%s | grep "ID"`
+else
+   changelog=`git log --pretty=format:%s $newest_tag...$second_newest_tag | grep "ID"`
+fi
 
-# write your script here
+date=`git log -1 --format=%ai $newest_tag`
+build_number=`echo $newest_tag | cut -d "_" -f 3`
 
-envman add --key COMMIT_MESSAGE_CLEAN --value "No commit messages."
-commit_messages_clean=`git log $(git describe --abbrev=0 --tags $(git describe --abbrev=0)^)...$(git describe --abbrev=0) --format=%B | grep -E '[ID]+-[0-9]+:.*[a-zA-Z]+'`
-envman add --key COMMIT_MESSAGE_CLEAN --value "$commit_messages_clean"
-exit 0
-
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-# envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
-
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+envman add --key COMMIT_MESSAGE_CLEAN --value "$changelog"
