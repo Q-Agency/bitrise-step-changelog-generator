adf51d010d875b5242c1d44cac321befe6cf1900
dmesek <dominik.mesek@q.agency>
2022-11-28 14:16:44 +0100

    modified step and desscription file

--- step.sh
+++ step.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
+# fail if any commands fails
 set -ex
 
-environment="staging"
-
-newest_tag=`git tag | grep $environment | tail -n 1`
+newest_tag=`git tag | grep $ENVIRNOMENT_TAG | tail -n 1`
 if [ -z $newest_tag ]; then
-   echo "No $environment tag found."
+   echo "No $ENVIRNOMENT_TAG tag found."
    exit 1
 fi
 
-tag_count=`git tag | grep $environment | wc -l`
-second_newest_tag=`git tag | grep $environment | tail -n 2 | head -n 1`
+tag_count=`git tag | grep $ENVIRNOMENT_TAG | wc -l`
+second_newest_tag=`git tag | grep $ENVIRNOMENT_TAG | tail -n 2 | head -n 1`
 if [ $tag_count -eq "1" ]; then
    changelog=`git log --pretty=format:%s | grep "ID"`
 else
-   changelog=`git log --pretty=format:%s $newest_tag...$second_newest_tag | grep "ID"`
+   changelog=`git log --pretty=format:%s $newest_tag...$second_newest_tag | grep "$COMMIT_TAG"`
 fi
 
 date=`git log -1 --format=%ai $newest_tag`
 build_number=`echo $newest_tag | cut -d "_" -f 3`
 
-envman add --key COMMIT_MESSAGE_CLEAN --value "$changelog"
+envman add --key CHANGELOG --value "$changelog"
